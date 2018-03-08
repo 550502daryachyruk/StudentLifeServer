07b8583b7e44b1097e4ab783abb323bcf85202a2
Nickolai  Shlyahtovich 550502 <nickolai.shlyahtovich@gmail.com>
2018-03-08 19:57:21 +0300

    Added names of mapping in entities.

--- src/Entity/League.php
+++ src/Entity/League.php
@@ -16,10 +16,25 @@ class League
      */
     private $id;
 
+
+    /**
+     * @ORM\OneToMany(targetEntity="League", mappedBy="parentLeague")
+     */
     private $childrenLeagues;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="League", inversedBy="childrenLeagues")
+     */
     private $parentLeague;
 
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Product", mappedBy="categories")
+     */
     private $admins;
+    /**
+     * @ORM\ManyToMany(targetEntity="Product", mappedBy="categories")
+     */
     private $users;
 
     private $events;
@@ -32,5 +47,68 @@ class League
         return $this->id;
     }
 
-    // add your own fields
+    /**
+     * @return mixed
+     */
+    public function getChildrenLeagues()
+    {
+        return $this->childrenLeagues;
+    }
+
+    /**
+     * @param mixed $childrenLeagues
+     */
+    public function setChildrenLeagues($childrenLeagues): void
+    {
+        $this->childrenLeagues = $childrenLeagues;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParentLeague()
+    {
+        return $this->parentLeague;
+    }
+
+    /**
+     * @param mixed $parentLeague
+     */
+    public function setParentLeague($parentLeague): void
+    {
+        $this->parentLeague = $parentLeague;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdmins()
+    {
+        return $this->admins;
+    }
+
+    /**
+     * @param mixed $admins
+     */
+    public function setAdmins($admins): void
+    {
+        $this->admins = $admins;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+
+    /**
+     * @param mixed $users
+     */
+    public function setUsers($users): void
+    {
+        $this->users = $users;
+    }
+
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -16,11 +16,50 @@ class User
      */
     private $id;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="League", inversedBy="admins")
+     */
     private $leaguesWhereAdmin;
+    /**
+     * @ORM\ManyToMany(targetEntity="League", inversedBy="users")
+     */
     private $leaguesWhereUser;
 
     private $alreadyPlayedEvent;
+
     private $willBePlayedEvent;
+
+    /**
+     * @return mixed
+     */
+    public function getLeaguesWhereAdmin()
+    {
+        return $this->leaguesWhereAdmin;
+    }
+
+    /**
+     * @param mixed $leaguesWhereAdmin
+     */
+    public function setLeaguesWhereAdmin($leaguesWhereAdmin): void
+    {
+        $this->leaguesWhereAdmin = $leaguesWhereAdmin;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLeaguesWhereUser()
+    {
+        return $this->leaguesWhereUser;
+    }
+
+    /**
+     * @param mixed $leaguesWhereUser
+     */
+    public function setLeaguesWhereUser($leaguesWhereUser): void
+    {
+        $this->leaguesWhereUser = $leaguesWhereUser;
+    }
 
 
     // add your own fields
